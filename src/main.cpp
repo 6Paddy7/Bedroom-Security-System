c7426706df2b3c6f25a6b6198f37c0eb374c70cf
6Paddy7 <patryk.ryczek@outlook.com>
2026-08-18 22:17:10 +0200

    Connecting matrix keypad

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,34 @@
 #include <Arduino.h> 
 #include <LiquidCrystal_I2C.h> 
+#include <Keypad.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const int trigPin = 10;
 const int echoPin = 11;
 
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', '%'},
+  {'4', '5', '6', '!'},
+  {'7', '8', '9', '@'},
+  {'*', '0', '#', '?'}
+};
+
+byte rowPins[ROWS] = {5, 4, 3, 2};
+byte colPins[COLS] = {6, 7, 8, 9};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+int cursorPosition = 0;
+char enteredCode[7];
+
 void setup() {
-  
+
+  char key = keypad.getKey();
+
   Serial.begin(9600);
 
   pinMode(trigPin, OUTPUT);
