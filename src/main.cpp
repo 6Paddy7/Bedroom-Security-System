a20465fbd59cc819afeeec644f2e04fd39281fb9
6Paddy7 <patryk.ryczek@outlook.com>
2026-08-18 22:35:09 +0200

    Updating the text displayed on the screen

--- src/main.cpp
+++ src/main.cpp
@@ -59,8 +59,10 @@ void loop() {
     delay(250);
 
     lcd.clear();
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0, 0);
     lcd.print("Intruder Detected!"); 
+    lcd.setCursor(0, 1);
+    lcd.print("Enter Pincode: ");
     
     delay(250);
 
